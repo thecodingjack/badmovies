138bfcb9870de9c8063fda6e1ca775a2fc3272cd
Philip Gonzalez <pgonzalez115@gmail.com>
2018-07-14 09:22:00 -0400

    fixed untracked files

--- server/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  host: 'localhost',
-  user: 'ur sql username which is probably root', 
-  password: 'ur sql password which is probably nothing', 
-  database: 'badmovies',
-  API_KEY: 'API_KEY_GOES_HERE',
-};
