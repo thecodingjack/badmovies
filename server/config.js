0e494b6878473d2f46d5b94800bfa16b63d4ef8c
Philip Gonzalez <p-gonzo@users.noreply.github.com>
2018-07-15 18:09:32 -0400

    Create config.js

---
+++ server/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  host: 'localhost',
+  user: 'ur sql username which is probably root', 
+  password: 'ur sql password which is probably nothing', 
+  database: 'badmovies',
+  API_KEY: 'API_KEY_GOES_HERE',
+};
